75ffcd4115d3c01ddb051de42b6d4957fa604266
Rofy.Ray <okyereforsonr@berea.edu>
2017-12-02 23:45:59 -0500

    Started coding the general structure of the game.

--- main_game.py
+++ main_game.py
@@ -14,3 +14,32 @@
 # Attribution-Noncommercial-Share Alike 3.0 United States License.
 ####################################################################################
 
+import tkinter          #Import the module needed for creating a GUI
+import random           #Import the module needed for generatinf random numbers
+
+class Gambit:
+    def __init__(self):
+        '''
+        The initializer creates a window to contain the widgets that will be used in playing the game
+        '''
+        self.color_pick = []            #List of possible colors to be picked
+        self.score_board = 0            #Sets the starting score of the user to 0 until the game begins
+        self.time_remain = 45           #Sets each game round timer to start at 45 secs
+        self.game_hub = tkinter.Tk()    #Creates the root window where all the widgets will be added
+
+
+    def __str__(self):
+        '''
+        Makes the str() function work with Gambits
+        :return: A formatted string for better printing
+        '''
+        return "()".format(self.color_pick)
+
+    def game_over(self):
+        '''
+
+        :return:
+        '''
+        if self.time_remain == 45:
+            countdown()
+
